0e4662b7bdf00ad064b18a545dbf8b79654e87d2
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-08 13:46:03 +0530

    added login structure with style

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,17 @@
 const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
   return (
     <>
-      <p>Hello</p>
+      <div className="login-container">
+        <form className="form" onSubmit={handleSubmit}>
+          <p>login</p>
+          <input type="text" placeholder="username"/>
+          <input type="password" placeholder="password" autoComplete="new-password" />
+          <button type="submit" className="submit">login</button>
+        </form>
+      </div>
     </>
   )
 }
