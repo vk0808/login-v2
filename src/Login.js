e7c7949b92633cdc6bb77f2c3f56033f25578cc2
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-08 20:44:13 +0530

    working with useState

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,80 @@
+import React, { useState, useEffect } from "react";
+import { login } from "./utils";
+
 const Login = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState({show: false, msg:""});
+
+  const showError = (show= false, msg = '' ) => {
+    setError({ show, msg })
   }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setIsLoading(true);
+    showError();
+
+    try {
+      await login({ username, password });
+
+      setIsLoggedIn(true);
+      showError();
+    } catch (error) {
+      showError(true, "incorrect password or username!");
+      setIsLoggedIn(false);
+    }
+    setIsLoading(false);
+    setUsername("");
+    setPassword("");
+  };
+  useEffect(() => {
+    const timeout = setTimeout(()=>{
+      showError()
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [error]);
   return (
     <>
       <div className="login-container">
-        <form className="form" onSubmit={handleSubmit}>
-          <p>login</p>
-          <input type="text" placeholder="username"/>
-          <input type="password" placeholder="password" autoComplete="new-password" />
-          <button type="submit" className="submit">login</button>
-        </form>
+        {isLoggedIn ? (
+          <>
+            <h1 className="greeting">welcome, {username}</h1>
+            <button className="submit" onClick={() => setIsLoggedIn(false)}>
+              logout
+            </button>
+          </>
+        ) : (
+          <form className="form" onSubmit={handleSubmit}>
+            {error.show && (
+              <p id="alert" className="error">
+                {error.msg}
+              </p>
+            )}
+            <p>login</p>
+            <input
+              type="text"
+              placeholder="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              autoComplete="new-password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit" className="submit" disabled={isLoading}>
+              {isLoading ? "logging in..." : "login"}
+            </button>
+          </form>
+        )}
       </div>
     </>
-  )
-}
+  );
+};
 export default Login;
